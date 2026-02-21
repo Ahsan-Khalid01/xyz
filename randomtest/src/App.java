52b95506587493ef4103d1f7db0b0da906eab7a9
Ahsan Khalid <ahsankhalid0968@gmail.com>
2026-02-21 19:38:25 +0500

    Add files via upload

---
+++ randomtest/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args)  {
+       // System.out.println("Hello, World!");
+       int a=5;
+       int b=a++ + ++a;
+       System.out.print(b);
+    }
+}
